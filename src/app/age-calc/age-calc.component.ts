e65f5f6f838802f2e9da2317dad5fd9c77964668
SHIV220 <55434267+SHIV220@users.noreply.github.com>
2023-06-20 21:05:16 +0530

    2 files modified, 1 added

--- src/app/age-calc/age-calc.component.ts
+++ src/app/age-calc/age-calc.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgeCalcComponent implements OnInit {
 
+  age:any;
+  showAge:any;
+
+  
+
   constructor() { }
+
 
   ngOnInit() {
   }
 
+  ageCalculator(){
+    if(this.age){
+      const convertAge = new Date(this.age);
+      const timeDiff = Math.abs(Date.now() - convertAge.getTime());
+      this.showAge = Math.floor((timeDiff / (1000 * 3600 * 24))/365);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AgeCalcComponent } from './age-calc/age-calc.component';
 import { AppComponent }  from './app.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule,FormsModule ],
   declarations: [ AppComponent , AgeCalcComponent],
   bootstrap:    [ AppComponent ]
 })
